1645d2a53c61c985a83163fdec687d3965e8102e
JonathanDudzik <jonathan.d.wesley@gmail.com>
2024-09-19 13:25:37 -0500

    refactor Babylon to classes

---
+++ articulate-3d-world-stats/story_content/triggers.js
@@ -0,0 +1,45 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "61yAqM343wS":
+        Script1();
+        break;
+      case "6cYaxwRkJ18":
+        Script2();
+        break;
+      case "6bCUXNYYEqa":
+        Script3();
+        break;
+      case "5uHGVByE2rH":
+        Script4();
+        break;
+      case "6ixFHUQQqM8":
+        Script5();
+        break;
+      case "6HXzkL45ua1":
+        Script6();
+        break;
+      case "5fRYkSntehL":
+        Script7();
+        break;
+      case "6EGfxtvMoKR":
+        Script8();
+        break;
+      case "6ZsmFu1pbKN":
+        Script9();
+        break;
+      case "5xql2NbS3Qb":
+        Script10();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
